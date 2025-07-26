9606a48d9ba358a9b6eef33cdd1b8ae843d27a8b
Mostafa Hagras <mostafahagras07@gmail.com>
2025-07-26 17:43:25 +0300

    lecture 7

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import Car from "./car";
 import { ControlType } from "./controls";
 import Road from "./road";
+import Visualizer from "./visualizer";
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas") as HTMLCanvasElement;
+carCanvas.width = 200;
+const netowrkCanvas = document.getElementById("networkCanvas") as HTMLCanvasElement;
+netowrkCanvas.width = 300;
 
-const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const carCtx = carCanvas.getContext("2d") as CanvasRenderingContext2D;
+const networkCtx = netowrkCanvas.getContext("2d") as CanvasRenderingContext2D;
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, ControlType.Ai);
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, ControlType.Dummy, 2),
@@ -18,14 +22,18 @@ function animate() {
     trafficCar.update(road.borders, []);
   }
   car.update(road.borders, traffic);
-  canvas.height = window.innerHeight;
-  ctx.save();
-  ctx.translate(0, canvas.height * 0.7 - car.y);
-  road.draw(ctx);
+  carCanvas.height = window.innerHeight;
+  netowrkCanvas.height = window.innerHeight;
+
+  carCtx.save();
+  carCtx.translate(0, carCanvas.height * 0.7 - car.y);
+  road.draw(carCtx);
   for (const trafficCar of traffic) {
-    trafficCar.draw(ctx, "red");
+    trafficCar.draw(carCtx, "red");
   }
-  car.draw(ctx, "blue");
-  ctx.restore();
+  car.draw(carCtx, "blue");
+  carCtx.restore();
+
+  Visualizer.drawNetwork(networkCtx, car.brain!)
   requestAnimationFrame(animate);
 }
